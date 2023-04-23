c4519f80b640037a9aa4a1cbb7d9a071501e46dc
Mauricio1408 <126628017+Mauricio1408@users.noreply.github.com>
2023-04-23 18:31:37 +0800

    Add files via upload

---
+++ #Homepage.py
@@ -0,0 +1,7 @@
+#Home[age]
+import streamlit as st
+
+st.header("Hello there, user!")
+st.subheader("This is an application for CCS221 - Computer Graphics and Visual Computing")
+st.subheader("It includes all the activities we made since Day 1")
+st.subheader("Enjoy!")
